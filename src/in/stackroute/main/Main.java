5e90183b08c3450dea04aafa5f7a6c34cddb4422
Mohammed Arfathulla Sadiq <arfathsadiq99@gmail.com>
2021-06-18 22:44:25 +0530

    Completed

--- src/in/stackroute/main/Main.java
+++ src/in/stackroute/main/Main.java
@@ -1,8 +1,36 @@
 package in.stackroute.main;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        // Add Your Code Here
+public class Main {
+        public static void main (String[]args)
+        {
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+            String s = Integer.toString(num);
+            int result = UserMainCode.checkSum(s);
+            if (result == -1)
+                System.out.println("Sum of odd digits is even.");
+            else {
+                System.out.println("Sum of odd digits is odd.");
+            }
+
+        }
+}
+
+class UserMainCode {
+
+    static int checkSum(String s){
+        char[] mychar = s.toCharArray();
+        int sum = 0;
+        for(int i=0; i<mychar.length; i++) {
+            if (i % 2 != 0)
+                sum += mychar[i];
+        }
+        if(sum%2 == 0)
+            return -1;
+        else
+            return 1;
     }
+
 }
